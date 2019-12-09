b8c740aa1b363e737283f96dec78256897e00ccb
njogukarungu <njogukarungu1@gmail.com>
2019-12-09 14:26:38 +0300

    Addedhover function

--- js/scripts.js
+++ js/scripts.js
@@ -12,31 +12,34 @@ $(document).ready(function(){
 $(".imgDetails .img1").click(function(){
     $(".details").show();
     $(".imgDetails .img1").hide();
-
 });
+
+$(".imgDetails .img1").mouseout(function() {
+  $(".details").hide();
+  $(".imgDetails .img1").show();
+});
+
 $(".imgDetails .img2").click(function(){
     $(".ddetails").show();
     $(".imgDetails .img2").hide();
-
 });
+
+$(".imgDetails .img2").mouseout(function() {
+  $(".ddetails").hide();
+  $(".imgDetails .img2").show();
+});
+
 $(".imgDetails .img3").click(function(){
     $(".dddetails").show();
     $(".imgDetails .img3").hide();
 });
-$(".imgDetails .img1").mouseout(function(){
-    $(".details").hide();
-    $(".imgDetails .img1").show();
 
+$(".imgDetails .img3").mouseout(function() {
+  $(".dddetails").hide();
+  $(".imgDetails .img3").show();
 });
-$(".imgDetails .img2").mouseout(function(){
-    $(".ddetails").hide();
-    $(".imgDetails .img2").show();
 
-});
-$(".imgDetails .img3").mouseout(function(){
-    $(".dddetails").hide();
-    $(".imgDetails .img3").show();
-});
+
 
 $(".myportifolio").hover(
   function() {
